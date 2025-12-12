055d2dbcd7757cf8df3473f17e67c50081d473fa
nzninkm <nazaniinkarimian@gmail.com>
2025-12-12 18:02:33 +0330

    Adding Base Template To Form

--- food/urls.py
+++ food/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
+    # add items
+    path('add',views.create_item,name='create_item'),
+    
 ]
