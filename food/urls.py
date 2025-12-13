d925f7436eb4808cffc14ad0f2c02a152e61aa2f
nzninkm <nazaniinkarimian@gmail.com>
2025-12-13 18:25:43 +0330

    Creating User Registration Form

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('item/',views.item,name='item'),
     # add items
     path('add',views.create_item,name='create_item'),
+    # edit
+    path('update/<int:id>',views.update_item,name='update_item'),
+    # Delete
+    path('delete/<int:id>/',views.delete_item,name='delete_item'),
     
 ]
