0ebed3c301761209d4e74bc677abd98ff7399ad6
Nazaniinkarimian@gmail.com <nazaniinkarimian@gmail.com>
2025-12-16 14:28:49 +0330

    automating user association

--- food/urls.py
+++ food/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
     # add items
-    path('add',views.create_item,name='create_item'),
+    path('add',views.CreateItem.as_view(),name='create_item'),
     # edit
     path('update/<int:id>',views.update_item,name='update_item'),
     # Delete
